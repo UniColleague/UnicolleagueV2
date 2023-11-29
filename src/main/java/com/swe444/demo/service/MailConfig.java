fb39d7fb51238210f77bf700e0c9aae18b28ab07
UniColleague <152321366+UniColleague@users.noreply.github.com>
2023-11-29 23:13:59 +0300

    Update MailConfig.java

--- src/main/java/com/swe444/demo/service/MailConfig.java
+++ src/main/java/com/swe444/demo/service/MailConfig.java
@@ -14,8 +14,8 @@ public class MailConfig {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com"); // Set your mail server host
         mailSender.setPort(587); // Set your mail server port
-        mailSender.setUsername("creprkill@gmail.com"); // Set your email username
-        mailSender.setPassword("wsorvpyyvrijbvlp"); // Set your email password
+        mailSender.setUsername("@gmail.com"); // Set your email username
+        mailSender.setPassword(""); // Set your email password
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
